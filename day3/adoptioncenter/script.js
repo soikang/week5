a98a47a87feb3e5111ec2f748570553cc4cdc2ab
Soi <skang0806@gmail.com>
2023-09-28 21:51:37 -0600

    moved the jquery to script.js

--- day3/adoptioncenter/script.js
+++ day3/adoptioncenter/script.js
@@ -27,17 +27,31 @@ $(document).ready(function () {
         var $imgAlt = $(this).attr('alt');
 
         // Clear previous content and add the clicked image to the lightbox
-        $('.box').empty().append(`<img src="${$imgSrc}" alt="${$imgAlt}">`);
+        $('.box').empty().append(`<div class="closeX">X</div><img src="${$imgSrc}" alt="${$imgAlt}">`);
         
         // Show the backdrop and lightbox
         $('.backdrop').css('display', 'block').animate({ 'opacity': '0.7' }, 300);
         $('.box').fadeIn();
+        console.log('Image clicked');
     });
 
+   
     // Close lightbox on close button or backdrop click
-    $('.closeX, .backdrop').click(function () {
+    $('.backdrop').click(function () {
+        closeLightbox();
+    });
+
+    // Handle "closeX" click event using event delegation
+    // when trying to handle both together in one, it didn't work. 
+    // event delegation to handle the click event for the "closeX" element within the "box" element. 
+    // When the "closeX" element is clicked, it triggers the closeLightbox function, which hides the lightbox and backdrop.
+    $('.box').on('click', '.closeX', function () {
+            closeLightbox();
+    });
+
+    function closeLightbox() {
         $('.box').fadeOut(function () {
             $('.backdrop').css('display', 'none').animate({ 'opacity': '0' }, 300);
         });
-    });
+    }
 });
